0e4b137fc9bd1f4f0b3dc427bd314c7c2ff2feb8
s22shadowl <jjerry409@gmail.com>
2025-08-13 17:49:29 +0800

    feat(types): define APIErrorCode enum

---
+++ types/api.ts
@@ -0,0 +1,9 @@
+export enum APIErrorCode {
+  InvalidInput = 'INVALID_INPUT',
+  InvalidCredentials = 'INVALID_CREDENTIALS',
+  PlayerNotFound = 'PLAYER_NOT_FOUND',
+  ResourceNotFound = 'RESOURCE_NOT_FOUND',
+  InternalServerError = 'INTERNAL_SERVER_ERROR',
+  ResultBackendNotConfigured = 'RESULT_BACKEND_NOT_CONFIGURED',
+  ServiceUnavailable = 'SERVICE_UNAVAILABLE',
+}
